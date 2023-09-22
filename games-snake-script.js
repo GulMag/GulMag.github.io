f42886d6c81b79ff2011dbbd65b9f4f08e45781c
maggi <magdalenabannholzer@gmail.com>
2023-09-22 11:00:58 -0400

    game

--- games-snake-script.js
+++ games-snake-script.js
@@ -1,6 +1,13 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX=13, foodY=10;
+let snakeX = 5, snakeY = 5;
+
+const changeFoodPosition = ()  => {
+    // Passing a random 1-30 value as food position
+    foodX = Math.floor(Math.random() * 30) +1;
+    foodY = Math.floor(Math.random() * 30) +1;
+}
 
 const initGame = () => {
     let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
