cb47a779fe260d18c269999484abf7469bb991c0
maggi <magdalenabannholzer@gmail.com>
2023-09-22 10:59:51 -0400

    game

--- games-snake-script.js
+++ games-snake-script.js
@@ -4,4 +4,6 @@ let foodX=13, foodY=10;
 
 const initGame = () => {
     let htmlMarkup = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
+    playBoard.innerHTML = htmlMarkup; 
 }
+initGame(); 
